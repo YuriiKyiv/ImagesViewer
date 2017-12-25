b0bf26478ff43aa971dc4544c2a4ab7acf244675
Yurii Trokhymchuk <trohumchyk@gmail.com>
2017-12-25 23:43:45 +0200

    TYVImagesLibraryViewController -> init

---
+++ TYVImagesViewer/ViewControllers/TYVImagesLibraryViewController/TYVImagesLibraryViewController.m
@@ -0,0 +1,22 @@
+//
+//  TYVImagesLibraryViewController.m
+//  TYVImagesViewer
+//
+//  Created by Yurii Trokhymchuk on 25.12.17.
+//  Copyright © 2017 Yurii Trokhymchuk. All rights reserved.
+//
+
+#import "TYVImagesLibraryViewController.h"
+
+@interface TYVImagesLibraryViewController ()
+
+@end
+
+@implementation TYVImagesLibraryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
