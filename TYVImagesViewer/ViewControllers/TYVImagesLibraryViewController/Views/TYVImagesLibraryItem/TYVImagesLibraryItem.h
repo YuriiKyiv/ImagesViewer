67350ee51f7250c11eb3f314a16f27df6e4d0e84
Yurii Trokhymchuk <trohumchyk@gmail.com>
2017-12-26 01:21:08 +0200

    TYVImagesLibraryItem -> init

---
+++ TYVImagesViewer/ViewControllers/TYVImagesLibraryViewController/Views/TYVImagesLibraryItem/TYVImagesLibraryItem.h
@@ -0,0 +1,15 @@
+//
+//  TYVImagesLibraryItem.h
+//  TYVImagesViewer
+//
+//  Created by Yurii Trokhymchuk on 26.12.17.
+//  Copyright © 2017 Yurii Trokhymchuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TYVImagesLibraryItem : NSCollectionViewItem
+
+@property (weak) IBOutlet NSImageView *contentImageView;
+
+@end
---
+++ TYVImagesViewer/ViewControllers/TYVImagesLibraryViewController/Views/TYVImagesLibraryItem/TYVImagesLibraryItem.m
@@ -0,0 +1,22 @@
+//
+//  TYVImagesLibraryItem.m
+//  TYVImagesViewer
+//
+//  Created by Yurii Trokhymchuk on 26.12.17.
+//  Copyright © 2017 Yurii Trokhymchuk. All rights reserved.
+//
+
+#import "TYVImagesLibraryItem.h"
+
+@interface TYVImagesLibraryItem ()
+
+@end
+
+@implementation TYVImagesLibraryItem
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
