6f888a8713fe5b8ace8e26598766af0307e67839
Yurii Trokhymchuk <trohumchyk@gmail.com>
2017-12-26 21:40:17 +0200

    TYVImagesLibraryWindowController -> init

---
+++ TYVImagesViewer/WindowControllers/TYVImagesLibraryWindowController/TYVImagesLibraryWindowController.h
@@ -0,0 +1,13 @@
+//
+//  TYVImagesLibraryWindowController.h
+//  TYVImagesViewer
+//
+//  Created by Yurii Trokhymchuk on 26.12.17.
+//  Copyright © 2017 Yurii Trokhymchuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TYVImagesLibraryWindowController : NSWindowController
+
+@end
